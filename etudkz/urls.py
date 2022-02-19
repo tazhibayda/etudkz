5492e4a00c0412b3e648b85a03d849d5ebd15276
Gulnaaaaaaz <200103502@stu.sdu.edu.kz>
2022-02-19 22:23:21 +0600

    lol

--- etudkz/urls.py
+++ etudkz/urls.py
@@ -7,3 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('courses/', include('main.urls')),
 ]
+
+
--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('' , views.course)
+    path('' , views.course),
+    path('asd', views.send )
 ]

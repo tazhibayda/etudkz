83a8fae4cd861a59b6dddc87424fb455eabfdc82
tazhibayda <200103210@stu.sdu.edu.kz>
2022-02-27 08:02:56 +0600

    Readme

--- etudkz/settings.py
+++ etudkz/settings.py
@@ -1,4 +1,4 @@
-
+from secret import *
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hx7c2s9f5tb(9=ah15@z3yxk%o=lnxuh=v#_^*4-776gp)8m=s'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

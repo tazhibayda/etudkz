c872fa98c5c55270b91017d372a1e049ac279b71
tazhibayda <200103210@stu.sdu.edu.kz>
2022-04-10 17:06:07 +0600

    ggg won!!!

--- main/models.py
+++ main/models.py
@@ -12,7 +12,7 @@ class Course(models.Model):
     # icon = models.ImageField(upload_to='images' , null=False)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     added = models.ManyToManyField(User, default=None)
-    # liked = models.BooleanField(False)
+    liked = models.BooleanField(False)
     def __str__(self):
         return f'{self.coursename}, Teacher: {self.teacher}, price: {self.price}  , {self.added.all()}'
 
@@ -34,10 +34,12 @@ class Comment(models.Model):
    text = models.TextField(max_length=1024)
    date = models.DateTimeField(auto_now_add=True)
    anonymous = models.BooleanField(default=False)
+   liked = models.ManyToManyField(User)
+   dis = models.ManyToManyField(User , related_name='dislike')
    # child = models.
    # parent = models.OneToOneField('self' , on_delete=models.CASCADE , null=True)
    def __str__(self):
-       return f'{self.pk} , {self.course_id} '
+       return f'{self.pk} , {self.course_id}'
 
 
 class Learning(models.Model):
